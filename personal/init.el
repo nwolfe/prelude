c36abc03604840e993e8e4c4eedaeee391598da0
Nate Wolfe <nwolfe@puppetlabs.com>
2014-09-11 12:38:22 -0700

    Enable git-gutter everywhere

--- personal/init.el
+++ personal/init.el
@@ -1,2 +1,8 @@
 ;; Disable the menu bar by default
 (menu-bar-mode -1)
+
+;; Enable git-gutter everywhere
+(global-git-gutter-mode +1)
+
+;; TODO auto-install my packages...
+;; (prelude-require-package '(git-gutter))
